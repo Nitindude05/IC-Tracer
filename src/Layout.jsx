476faba439fc939d47ecd1fad99bd59d900605a5
Nitin Khasdeo <nitinkhasdeo05@gmail.com>
2025-10-15 23:25:34 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer.jsx";
 import CoreServices from "./Components/CoreServices";
 import CustomerSection from "./Constants/CustomerSections"
 import MagicNumbers from "./Constants/MagicNumbers";
